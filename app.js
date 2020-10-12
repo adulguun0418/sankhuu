4430d0a6d4851c4f3c41c2985bf01e27ec4d3186
Dulguun A <adulguun0418@gmail.com>
2020-10-13 01:54:06 +0900

    added event listener

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var uiController = (function () {
-  var x = 17;
-})();
+var uiController = (function () {})();
 
-var financeController = (function () {
-  var y = 7;
-})();
+var financeController = (function () {})();
 
-var appController = (function (uiController, financeControllerler) {})(
-  uiController,
-  financeController
-);
+var appController = (function (uiController, financeControllerler) {
+  var ctrlAddItem = function () {
+    console.log("gotya");
+    //find input data
+    //pass the data to the financeController
+    //show the data on the web
+    //calculate budget
+    //show the final amount
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
